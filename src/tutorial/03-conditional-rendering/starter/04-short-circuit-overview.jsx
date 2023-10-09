46a9a951d46f62e1dd6d0705116a91931f5aacc4
Mairaj Pirzada <thisismairaj@gmail.com>
2023-10-09 12:14:05 +0500

    short circuit examples

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react"
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
-};
-export default ShortCircuitOverview;
+  const [name, setName] = useState("")
+  const [text, setText] = useState("susan")
+  return (
+    <div>
+      {/* {if(text){"won't work"}} */}
+      <h2>Falsy &&: {name && "hi"}</h2>
+      <h2>Falsy OR: {name || "hi"}</h2>
+      <h2>Truthy &&: {text && "hi"}</h2>
+      <h2>Truthy OR: {text || "hi"}</h2>
+    </div>
+  )
+}
+export default ShortCircuitOverview
--- src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react"
 
 const ShortCircuitExamples = () => {
   // falsy
-  const [text, setText] = useState('');
+  const [text, setText] = useState("")
   // truthy
-  const [name, setName] = useState('susan');
-  const [user, setUser] = useState({ name: 'john' });
-  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState("susan")
+  const [user, setUser] = useState({ name: "john" })
+  const [isEditing, setIsEditing] = useState(false)
 
-  return <h2>short circuit - examples</h2>;
-};
+  return (
+    <div>
+      {/* {if(text){"won't work"}} */}
+      <h2>Falsy OR: {name || "hi"}</h2>
+    </div>
+  )
+}
 
-export default ShortCircuitExamples;
+export default ShortCircuitExamples
